38bdeefc8c275c78bb8ca28dc1f64a5d043393c4
Meet Vekariya <163901571+mpatel184@users.noreply.github.com>
2025-08-08 12:53:37 +0530

    Update app.py add Backend url

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 # from exception.exceptions import TradingBotException
 import sys
 
-BASE_URL = "http://localhost:8000"  # Backend endpoint
+BASE_URL = "https://ai-trip-planner-4shb.onrender.com"  # Backend endpoint
 
 st.set_page_config(
     page_title="🌍 Travel Planner Agentic Application",
